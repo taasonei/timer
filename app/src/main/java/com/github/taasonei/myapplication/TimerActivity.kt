6e8b71c8587879fdfdac6920d3fa32cd53f2933a
Anna <taasonei@gmail.com>
2022-04-29 02:46:25 +0300

    created timer logic based on tutorial

--- app/src/main/java/com/github/taasonei/myapplication/TimerActivity.kt
+++ app/src/main/java/com/github/taasonei/myapplication/TimerActivity.kt
@@ -1,12 +1,26 @@
 package com.github.taasonei.myapplication
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.Menu
 import android.view.MenuItem
+import android.view.TextureView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.taasonei.myapplication.databinding.ActivityTimerBinding
+import com.github.taasonei.myapplication.util.PrefUtil
+import me.zhanghai.android.materialprogressbar.MaterialProgressBar
 
 class TimerActivity : AppCompatActivity() {
+
+    enum class TimerState {
+        Stopped, Paused, Running,
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds = 0L
+    private var secondsRemaining = 0L
+    private var timerState = TimerState.Stopped
 
     private lateinit var binding: ActivityTimerBinding
 
@@ -20,6 +34,130 @@ class TimerActivity : AppCompatActivity() {
         supportActionBar?.setIcon(R.drawable.ic_timer)
         supportActionBar?.title = "    Timer"
 
+        binding.fabStart.setOnClickListener {
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
+        }
+
+        binding.fabPause.setOnClickListener {
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+
+        binding.fabStop.setOnClickListener {
+            timer.cancel()
+            onTimerFinished()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initTimer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (timerState == TimerState.Running) {
+            timer.cancel()
+        } else if (timerState == TimerState.Paused) {
+
+        }
+
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
+    }
+
+    private fun initTimer() {
+        timerState = PrefUtil.getTimerState(this)
+
+        if (timerState == TimerState.Stopped)
+            setNewTimerLength()
+        else
+            setPreviousTimerLength()
+
+        secondsRemaining = if (timerState == TimerState.Running || timerState == TimerState.Paused)
+            PrefUtil.getSecondsRemaining(this)
+        else
+            timerLengthSeconds
+
+        if (timerState == TimerState.Running)
+            startTimer()
+
+        updateButtons()
+        updateCountdownUi()
+    }
+
+    private fun onTimerFinished() {
+        timerState = TimerState.Stopped
+
+        setNewTimerLength()
+
+        val progressCountdown = findViewById<MaterialProgressBar>(R.id.progress_countdown)
+        progressCountdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+
+        updateButtons()
+        updateCountdownUi()
+    }
+
+    private fun startTimer() {
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinised: Long) {
+                secondsRemaining = millisUntilFinised / 1000
+                updateCountdownUi()
+            }
+        }.start()
+    }
+
+    private fun setNewTimerLength() {
+        val lengthInMinutes = PrefUtil.getTimerLength(this)
+        timerLengthSeconds = lengthInMinutes * 60L
+        findViewById<MaterialProgressBar>(R.id.progress_countdown).max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength() {
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(this)
+        findViewById<MaterialProgressBar>(R.id.progress_countdown).max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUi() {
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinutesUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinutesUntilFinished.toString()
+
+        findViewById<TextView>(R.id.textview_countdown).text =
+            "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0" + secondsStr}"
+        findViewById<MaterialProgressBar>(R.id.progress_countdown).progress =
+            (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+    private fun updateButtons() {
+        when (timerState) {
+            TimerState.Running -> {
+                binding.fabStart.isEnabled = false
+                binding.fabPause.isEnabled = true
+                binding.fabStop.isEnabled = true
+            }
+            TimerState.Paused -> {
+                binding.fabStart.isEnabled = true
+                binding.fabPause.isEnabled = false
+                binding.fabStop.isEnabled = true
+            }
+            TimerState.Stopped -> {
+                binding.fabStart.isEnabled = true
+                binding.fabPause.isEnabled = false
+                binding.fabStop.isEnabled = false
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
